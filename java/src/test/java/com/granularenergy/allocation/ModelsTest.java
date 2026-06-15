a97c0d270f8958b64e88f32cc93f2b0f60e3e2e8
Gareth Thackeray <gareth.thackeray@granular-energy.com>
2026-06-15 11:32:47 +0100

    Convert Java to use doubles instead of decimals. Makes it easier to use and less confusing as standard operators can be used and division works as expected

--- java/src/test/java/com/granularenergy/allocation/ModelsTest.java
+++ java/src/test/java/com/granularenergy/allocation/ModelsTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import com.granularenergy.allocation.models.ConsumptionVolume;
 import com.granularenergy.allocation.models.Duration;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
@@ -19,12 +18,12 @@ class ModelsTest {
             "C1",
             LocalDateTime.of(2022, 1, 1, 0, 0).atOffset(ZoneOffset.UTC).toLocalDateTime(),
             Duration.HOUR,
-            new BigDecimal("4671.549")
+            4671.549
         );
 
         assertEquals("C1", volume.consumer());
         assertEquals(LocalDateTime.of(2022, 1, 1, 0, 0), volume.periodStart());
         assertEquals(Duration.HOUR, volume.duration());
-        assertEquals(new BigDecimal("4671.549"), volume.quantityKwh());
+        assertEquals(4671.549, volume.quantityKwh());
     }
 }
